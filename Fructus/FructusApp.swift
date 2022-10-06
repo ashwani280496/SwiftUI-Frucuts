e5dd3adcb7a4d4177f2bf59e6c841345f6a74aa4
Ashwani Shakya <ashwani.kumar@theporter.in>
2022-10-06 08:53:37 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Ashwani Kumar on 06/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
